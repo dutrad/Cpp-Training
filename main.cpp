458a10d6032c6fa424c769ab8b5d3e31cca2b6c6
Luiz Silva <lcesarfs@hotmail.com>
2025-07-17 11:31:32 +0100

    inserting face detection

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 #include <iostream>
 #include <stdio.h>
 
@@ -9,16 +10,25 @@ int main()
 {
     cv::Mat frame;
     cv::VideoCapture cap;
+    cv::CascadeClassifier face_cascade;
+
     int deviceID = 0;
     int apiID = cv::CAP_ANY;
     cap.open(deviceID, apiID);
+
     if (!cap.isOpened()) {
         std::cerr << "ERROR! Unable to open camera\n";
+        return -1;
+    }
+        
+    if (!face_cascade.load("haarcascade_frontalface_default.xml")) {
+        std::cerr << "ERROR! Unable to load face cascade\n";
         return -1;
     }
  
     std::cout << "Start grabbing" << std::endl
         << "Press any key to terminate" << std::endl;
+
     while(true) {
         cap.read(frame);
 
@@ -26,7 +36,12 @@ int main()
             std::cerr << "ERROR! blank frame grabbed\n";
             break;
         }
-        cv::cvtColor(frame, frame, cv::COLOR_RGB2GRAY);
+        std::vector<cv::Rect> faces;
+        face_cascade.detectMultiScale(frame, faces, 1.1, 3, 0);
+
+        for (const auto& face : faces) {
+            cv::rectangle(frame, face, cv::Scalar(255, 0, 0), 2);
+        }
 
         cv::imshow("Live", frame);
         if (cv::waitKey(5) >= 0)
